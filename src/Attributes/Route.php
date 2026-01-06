6d24cae368956c129ae6787c65ed2b67ac554269
MrYamous <matt.lempereur@gmail.com>
2026-01-06 07:04:52 +0100

    chore: add phpstan and apply rules

--- src/Attributes/Route.php
+++ src/Attributes/Route.php
@@ -5,6 +5,12 @@ namespace Yamous\LivewireRouteAttributes\Attributes;
 #[\Attribute(\Attribute::TARGET_CLASS | \Attribute::IS_REPEATABLE)]
 class Route
 {
+    /**
+     * @param string $uri
+     * @param string $name
+     * @param string|string[] $methods
+     * @param string[] $middleware
+     */
     public function __construct(
         public string $uri,
         public array|string $methods = [],
